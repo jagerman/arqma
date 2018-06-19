b7e18b266e6f7c984121bf0e2273548f1ba8fcad
Michal vel m@lbit <35476352+malbit@users.noreply.github.com>
2018-06-19 02:51:01 +0100

    Update transaction_history.h

--- src/wallet/api/transaction_history.h
+++ src/wallet/api/transaction_history.h
@@ -31,7 +31,7 @@
 #include "wallet/api/wallet2_api.h"
 #include <boost/thread/shared_mutex.hpp>
 
-namespace Arqma {
+namespace Monero {
 
 class WalletImpl;
 
@@ -56,5 +56,5 @@ private:
 
 }
 
-namespace Arqma = Arqma;
+namespace Bitmonero = Monero;
 
